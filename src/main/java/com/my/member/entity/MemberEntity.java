75a5dc35c6ee369eec2a3f7174cc50c028cd6825
aeokseung <oksh3012@naver.com>
2023-11-23 16:46:54 +0900

    Feat: chat, member, department의 entity 추가

---
+++ src/main/java/com/my/member/entity/MemberEntity.java
@@ -0,0 +1,39 @@
+package com.my.member.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+import com.my.department.entity.DepartmentEntity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class MemberEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long memberId;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private String position;
+
+	@Column(nullable = false, unique = true)
+	private String tel;
+
+	@ManyToOne
+	@JoinColumn(name = "departmentId")
+	private DepartmentEntity department;
+}
