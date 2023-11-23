ac7c96d70ab28e096b541d6e97b385c058b80b19
nawonhee <nwh0921@naver.com>
2023-11-23 20:09:21 +0900

    [FEAT] Schedule Entity, DTO 추가

--- src/main/java/com/my/schedule/entity/ScheduleEntity.java
+++ src/main/java/com/my/schedule/entity/ScheduleEntity.java
@@ -1,5 +1,48 @@
 package com.my.schedule.entity;
 
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+
+import com.my.member.entity.MemberEntity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+
+@Entity
+@DynamicInsert
+@Table(name="schedule")
+@SequenceGenerator(name="schedule_seq_generator", sequenceName="schedule_seq", initialValue=1, allocationSize=1)
 public class ScheduleEntity {
-	//t
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "schedule_seq_generator")
+	private Long scheduleId;
+	
+	@ManyToOne
+	@JoinColumn(name="memberId", nullable=false)
+	private MemberEntity member;
+	
+	@Column(nullable=false, length=100)
+	private String title;
+	
+	@Column(length=200)
+	private String content;
+	
+	private Date startDate;
+	
+	private Date endDate;
 }
