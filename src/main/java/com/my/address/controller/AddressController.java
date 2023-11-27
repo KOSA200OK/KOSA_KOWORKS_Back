ed49c1edf7aad7a64a82539589379a6673900dbc
aeokseung <oksh3012@naver.com>
2023-11-27 17:37:10 +0900

    Feat: address 수정

--- src/main/java/com/my/address/controller/AddressController.java
+++ src/main/java/com/my/address/controller/AddressController.java
@@ -3,6 +3,8 @@ package com.my.address.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +22,7 @@ public class AddressController {
 		this.addressService = addressService;
 	}
 
+	@CrossOrigin(origins = "http://localhost:5173")
 	@GetMapping("/members") // /address/members에 대한 매핑 추가
 	public List<AddressMemberDTO> getAllMembers() {
 		return addressService.getAllMembers();
