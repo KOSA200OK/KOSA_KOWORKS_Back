b682f074d5f8183d752118192add310125aeb919
오찬석 <dhcksehf1@naver.com>
2023-11-27 15:34:40 +0900

    [feat] attendance 출석, 퇴근 메서드 구현

--- src/main/java/com/my/attendance/control/AttendanceController.java
+++ src/main/java/com/my/attendance/control/AttendanceController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import com.my.attendance.entity.AttendanceEntity;
 import com.my.attendance.service.AttendanceService;
 import com.my.exception.AddException;
 import com.my.exception.FindException;
+import com.my.exception.ModifyException;
 
 @RestController
 @RequestMapping("/attendance")
@@ -43,5 +45,18 @@ public class AttendanceController {
 		}
 		
 	} // createAttendance
+	
+	@PutMapping()
+	public ResponseEntity<?> updateAttendance(@RequestBody AttendanceDTO dto) throws ModifyException {
+		
+		try {
+			service.updateAttendance(dto);
+			
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (ModifyException e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		
+	} // updateAttendance
 	
 } // end class
