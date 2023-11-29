543d698dcdce680482c995924b5ed1dd7c1b4a09
ykchoi7 <7dbsrud7@gmail.com>
2023-11-29 13:51:25 +0900

    [Feat] MeetingRoomReservation_Back구현

--- src/main/java/com/my/meetingroom/repository/MeetingReservationRepository.java
+++ src/main/java/com/my/meetingroom/repository/MeetingReservationRepository.java
@@ -1,7 +1,7 @@
 package com.my.meetingroom.repository;
 
-import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -15,4 +15,10 @@ public interface MeetingReservationRepository extends JpaRepository<MeetingReser
 			+ "ORDER BY md.id",
 			nativeQuery=true)
 	public List<MeetingReservationEntity> findAllMeetingRoom(String meetingDate);
+	
+	@Query(value="SELECT * \r\n"
+			+ "FROM meeting_reservation mr JOIN member m ON mr.member_id = m.id\r\n"
+			+ "WHERE mr.id = :id",
+			nativeQuery=true)
+	public Optional<MeetingReservationEntity> findById(Long id);
 }
--- src/main/java/com/my/meetingroom/service/MeetingroomServiceImpl.java
+++ src/main/java/com/my/meetingroom/service/MeetingroomServiceImpl.java
@@ -5,10 +5,12 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.my.exception.AddException;
 import com.my.exception.FindException;
 import com.my.meetingroom.dto.MeetingReservationDTO;
 import com.my.meetingroom.entity.MeetingReservationEntity;
@@ -26,23 +28,37 @@ public class MeetingroomServiceImpl implements MeetingroomService {
 	
 	@Autowired
 	MeetingReservationRepository reservation;
-	
+		
 	@Override
 	public List<MeetingReservationDTO> findAllMeetingRoom(String meetingDate) throws FindException {
-		
 //		meetingDate = "2023-11-30";
 //		System.out.println(meetingDate);
 		
 		List<MeetingReservationEntity> entity = reservation.findAllMeetingRoom(meetingDate);
 		List<MeetingReservationDTO> list = new ArrayList();
-		MeetingroomMapper mm = new MeetingroomMapper();
+		MeetingroomMapper mapper = new MeetingroomMapper();
 		
 		//Vo->DTO
 		for (MeetingReservationEntity mre : entity) {
-			MeetingReservationDTO dto = mm.Reservation_VoToDto(mre);
+			MeetingReservationDTO dto = mapper.Reservation_VoToDto(mre);
 			list.add(dto);
 		}
 		return list;
+	}
+
+	@Override
+	public Optional<MeetingReservationDTO> findById(Long id) throws FindException {
+		Optional<MeetingReservationEntity> entity = reservation.findById(id);
+		MeetingroomMapper mapper = new MeetingroomMapper();
+		return entity.map(mapper::Reservation_VoToDto);
+	}
+
+	@Override
+	public void createMeetingReservation(MeetingReservationDTO msdto) throws AddException {
+		//DTO->Vo
+		MeetingroomMapper mapper = new MeetingroomMapper();
+		MeetingReservationEntity entity = mapper.Reservation_DtoToVo(msdto);
+		reservation.save(entity);
 	}
 
 }
