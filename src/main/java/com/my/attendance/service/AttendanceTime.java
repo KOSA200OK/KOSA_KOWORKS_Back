6b45b005ce98df66aa91bd1b9cd2ba75a641f748
오찬석 <dhcksehf1@naver.com>
2023-11-29 16:42:31 +0900

    [DOCS] attendance 주석 삭제 및 정리

--- src/main/java/com/my/attendance/service/AttendanceTime.java
+++ src/main/java/com/my/attendance/service/AttendanceTime.java
@@ -4,10 +4,10 @@ import java.time.LocalTime;
 
 public enum AttendanceTime {
 
-	ON_TIME(LocalTime.of(9, 0, 0)), 
-	OFF_TIME(LocalTime.of(17,0, 0)),
-	LATE_TIME(LocalTime.of(12, 0, 0)),
-	ABSENCE_TIME(LocalTime.of(12, 1, 0));
+	ON_TIME(LocalTime.of(9, 0, 0)), 			// 출근
+	OFF_TIME(LocalTime.of(17,0, 0)),			// 퇴근
+	LATE_TIME(LocalTime.of(12, 0, 0)),			// 지각
+	ABSENCE_TIME(LocalTime.of(12, 1, 0));		// 결근
 
     LocalTime time; // 첫 번째 시간 (시작 시간)
 //    LocalTime endTime; // 두 번째 시간 (종료 시간)
