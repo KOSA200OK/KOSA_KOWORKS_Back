82ca9855eaf51c563a3768e2b3bb011f3d625a6a
Logan-CatKeeper <142392396+Logan-CatKeeper@users.noreply.github.com>
2023-11-30 11:41:41 +0900

    feat 매퍼 및 비품요청, 비품요청목록 완료

---
+++ src/main/java/com/my/stuff/dto/StuffDTO.java
@@ -0,0 +1,17 @@
+package com.my.stuff.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StuffDTO {
+	private String id;
+    private String name;
+    private Long stock;
+}
