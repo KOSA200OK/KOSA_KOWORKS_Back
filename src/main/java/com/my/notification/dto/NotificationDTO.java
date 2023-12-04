62167f9829a9b45ec3b2e55be155ed02968c45e4
오찬석 <dhcksehf1@naver.com>
2023-12-04 11:47:07 +0900

    [feat] 차량예약 등록 알림 구현

--- src/main/java/com/my/notification/dto/NotificationDTO.java
+++ src/main/java/com/my/notification/dto/NotificationDTO.java
@@ -11,25 +11,25 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@ToString
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder
+//@ToString
 public class NotificationDTO {
 
-	private Integer id; 		// 알림번호
-	
-	private String memberId;			// 알림받는 사원 번호
-	
-	private String type;				// 알림타입
-	
-	private String content;				// 내용
-	
-	private Date createdAt;				// 알림생성일시
-	
-	private Integer status;				// 상태 0: 확인, 1: 미확인
+//	private Integer id; 		// 알림번호
+//	
+//	private String memberId;			// 알림받는 사원 번호
+//	
+//	private String type;				// 알림타입
+//	
+//	private String content;				// 내용
+//	
+//	private Date createdAt;				// 알림생성일시
+//	
+//	private Integer status;				// 상태 0: 확인, 1: 미확인
 	
 	@Getter
 	@Setter
@@ -48,7 +48,6 @@ public class NotificationDTO {
 					.content(notify.getContent())
 					.id(notify.getId().toString())
 					.memberId(notify.getMemberEntity().getName())
-					.createdAt(notify.getCreatedAt().toString())
 					.build();
 		}
 	}
