d7423f58b7c800beeb65cad1b958ebb6e2fb459a
오찬석 <dhcksehf1@naver.com>
2023-12-11 16:59:35 +0900

    [feat] 차량 승인,반려 알림 완료

--- src/main/java/com/my/car/service/CarServiceImpl.java
+++ src/main/java/com/my/car/service/CarServiceImpl.java
@@ -92,7 +92,7 @@ public class CarServiceImpl implements CarService {
 		log.warn("여기까진 오나..?");
 		
 	    // 찬석
-	    notify.send(memberEntity, NotificationEntity.NotificationType.CAR, "차량이 등록 되었습니다.");
+	    notify.send(memberEntity, NotificationEntity.NotificationType.CAR, "차량이 예약 되었습니다.");
 
 	}
 	
@@ -164,7 +164,7 @@ public class CarServiceImpl implements CarService {
 		return entityList.map(cm::entityToDto);
 	}
 	
-	//***************** 차량 관리 승인 **************************
+	//***************** 차량 	 승인 **************************
 	
 	@Override
 	public Page<CarRentDTO> findAllWaiting(Pageable pageable){
