0aa571ab785d4d161f794da4527f755f518380d7
ykchoi7 <7dbsrud7@gmail.com>
2023-12-11 17:51:59 +0900

    [Feat] 회의실예약 - 참여자추가 수정

--- src/main/java/com/my/meetingroom/service/MeetingroomServiceImpl.java
+++ src/main/java/com/my/meetingroom/service/MeetingroomServiceImpl.java
@@ -158,12 +158,13 @@ public class MeetingroomServiceImpl implements MeetingroomService {
 	}
 
 	@Override
-	public void createParticipants(ParticipantsDTO pdto) throws AddException {		
+	public void createParticipants(List<ParticipantsDTO> pdtoli) throws AddException {		
 		//DTO->Vo
-		
 		MeetingroomMapper mapper = new MeetingroomMapper();
-		ParticipantsEntity pentity = mapper.Participants_DtoToVo(pdto);
-		ParticipantsEntity savedEntity = participants.save(pentity);
+		for (ParticipantsDTO pdto : pdtoli) {
+			ParticipantsEntity pentity = mapper.Participants_DtoToVo(pdto);			
+			ParticipantsEntity savedEntity = participants.save(pentity);
+		}
 	}
 
 	@Override
